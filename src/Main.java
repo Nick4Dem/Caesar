a6d168ca96ae272301393cf8dd5dd74d0e98cf62
Nick_Dem <nick_dem@ukr.net>
2023-07-29 00:06:28 +0300

    added new code

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,38 @@
-public class Main {
-    public static void main(String[] args) {
+import java.util.Scanner;
 
-        System.out.println("Hello world!");
-        System.out.println("Hellogg world!");
+public class Main {
+    private static final String al_ua = "йцукенгшщзхїфівапролджєячсмитьбю,.':-!?ЙЦУКЕНГШЩЗХЇФІВАПРОЛДЖЄЯЧСМИТЬБЮ";
+    private static final String al_en = "qwertyuiopasdfghjklzxcvbnm,.':-!?QWERTYUIOPASDFGHJKLZXCVBNM";
+    public static void main(String[] args) {
+        System.out.println("Оберіть з якою мовою бажаєте продовжувати? 1 - Українська чи 2 - Англійська");
+        Scanner scanner = new Scanner(System.in);
+        int var = scanner.nextInt();
+        String alph;
+        if (var==1) {
+            alph=al_ua;
+        } else {
+            alph=al_en;
+        }
+        System.out.println("Укажіть шлях до файлу:");
+        String path = scanner.nextLine();
+        System.out.println("Оберіть дію: 1 - Шифруємо файл, 2 - Дешифруємо");
+        int var = scanner.nextInt();
+        if (var==1) {
+            System.out.println("Укажіть ключ шифрування:");
+            int key = scanner.nextInt();
+            encrypt(path, key);
+        } else {
+            System.out.println("Укажіть ключ дешифрування, якщо 0 то буде виконо метобом брут-форсу:");
+            int key = scanner.nextInt();
+            decrypt(path, key);
+        }
+        System.out.println("Перевыряйте, зроблено!");
+    }
+
+    private static void encrypt(String alph, String path, int key){
+
+    }
+    private static void decrypt(String alph, String path, int key){
+
     }
 }
